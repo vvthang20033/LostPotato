de3576b095e3832736182c93061d6f6743540664
vvthang20033 <thang190420033@gmail.com>
2025-03-05 21:04:00 +0700

    new scene

---
+++ Assets/AssetsLostPotato - (1)/Assets -/Scripts/PlayVideo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        // Bắt đầu phát video khi Scene khởi tạo
+        videoPlayer.Play();
+    }
+
+
+}
