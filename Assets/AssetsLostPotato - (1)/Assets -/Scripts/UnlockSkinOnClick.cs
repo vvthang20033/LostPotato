066c69278718e0000f63032f3fd7fe590fa80380
Tên của bạn <email@example.com>
2025-03-16 02:28:26 +0700

    UnlockSkinOnClick

---
+++ Assets/AssetsLostPotato - (1)/Assets -/Scripts/UnlockSkinOnClick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class UnlockSkinOnClick : MonoBehaviour
+{
+
+    public GameObject Hat; // GameObject của nhân vật
+    public string animationName; // Tên animation cần chạy
+
+    private void OnMouseDown()
+    {
+        Animator animator = Hat.GetComponent<Animator>();
+        animator.Play(animationName);
+    }
+}
