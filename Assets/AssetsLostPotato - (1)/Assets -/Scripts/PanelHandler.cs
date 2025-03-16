fd5d82ab33d183f8d8086fe9c4766910cece9d71
Tên của bạn <email@example.com>
2025-03-16 18:10:51 +0700

    Update panel move and panel attack

--- Assets/AssetsLostPotato - (1)/Assets -/Scripts/PanelHandler.cs
+++ Assets/AssetsLostPotato - (1)/Assets -/Scripts/PanelHandler.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PanelHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class PanelHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerMoveHandler
 {
     public GameObject joystickPrefab; // Prefab của joystick
     private GameObject currentJoystick; // Joystick hiện tại
@@ -17,6 +17,26 @@ public class PanelHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
             out Vector2 localPoint
         );
         currentJoystick.GetComponent<RectTransform>().anchoredPosition = localPoint;
+
+        // Gán sự kiện cho joystick
+        Joystick joystick = currentJoystick.GetComponentInChildren<Joystick>();
+        if (joystick != null)
+        {
+            joystick.OnPointerDown(eventData);
+        }
+    }
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        // Cập nhật vị trí joystick khi di chuyển chuột
+        if (currentJoystick != null)
+        {
+            Joystick joystick = currentJoystick.GetComponentInChildren<Joystick>();
+            if (joystick != null)
+            {
+                joystick.OnDrag(eventData);
+            }
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -24,7 +44,14 @@ public class PanelHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
         // Hủy joystick khi thả chuột
         if (currentJoystick != null)
         {
+            Joystick joystick = currentJoystick.GetComponentInChildren<Joystick>();
+            if (joystick != null)
+            {
+                joystick.OnPointerUp(eventData);
+            }
             Destroy(currentJoystick);
         }
+        JoystickMove.directionMove = Vector2.zero;
+        JoystickAttack.directionAttack = Vector2.zero;
     }
 }
